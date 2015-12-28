86bade0898b1bdbd2a979294f372834036c1a827
LemADEC <lemadec.fr@gmail.com>
2015-12-28 18:32:33 +0100

    Code cleanup

--- src/main/java/dustmod/defaults/runes/RELiftTerrain.java
+++ src/main/java/dustmod/defaults/runes/RELiftTerrain.java
@@ -34,73 +34,76 @@ public class RELiftTerrain extends RuneEvent
     }
 	
 	@Override
-    public void initGraphics(EntityRune e){
-    	super.initGraphics(e);
+    public void initGraphics(EntityRune entityRune){
+    	super.initGraphics(entityRune);
 
-		e.setRenderBeam(true);
-        e.setColorStar(255, 255, 0);
-        e.setColorFire(0,0,255);
+		entityRune.setRenderBeam(true);
+        entityRune.setColorStar(255, 255, 0);
+        entityRune.setColorFire(0,0,255);
         
-        loadArea(e);
-		e.setRenderFireOnRuts(true);
+        loadArea(entityRune);
+		entityRune.setRenderFireOnRuts(true);
 		
     }
 
-    public void onInit(EntityRune e)
+    public void onInit(EntityRune entityRune)
     {
         ItemStack[] req = new ItemStack[] {new ItemStack(Blocks.red_flower, 1)};
-        req = this.sacrifice(e, req);
+        req = this.sacrifice(entityRune, req);
 
-        if (!checkSacrifice(req) || !takeXP(e, 10))
+        if (!checkSacrifice(req) || !takeXP(entityRune, 10))
         {
-            e.fizzle();
+            entityRune.fizzle();
             return;
         }
 
-		e.setRenderBeam(true);
-        e.setColorStar(255, 255, 0);
+		entityRune.setRenderBeam(true);
+        entityRune.setColorStar(255, 255, 0);
         int a, b, c, d;
 
-        if (e.dusts.length > e.dusts[0].length)
+        if (entityRune.dusts.length > entityRune.dusts[0].length)
         {
-            a = e.dusts[7][3];
-            b = e.dusts[8][3];
-            c = e.dusts[8][4];
-            d = e.dusts[7][4];
+            a = entityRune.dusts[7][3];
+            b = entityRune.dusts[8][3];
+            c = entityRune.dusts[8][4];
+            d = entityRune.dusts[7][4];
         }
         else
         {
-            a = e.dusts[3][7];
-            b = e.dusts[3][8];
-            c = e.dusts[4][8];
-            d = e.dusts[4][7];
+            a = entityRune.dusts[3][7];
+            b = entityRune.dusts[3][8];
+            c = entityRune.dusts[4][8];
+            d = entityRune.dusts[4][7];
         }
 
         int dustStrength = a;
 
         if (a != b || b != c || c != d)
         {
-            e.fizzle();
+            entityRune.fizzle();
             return;
         }
 
         switch (dustStrength)
         {
             case 100:
-                e.data[1] = 12;
+                entityRune.data[1] = 12;
                 break;
 
             case 200:
-                e.data[1] = 16;
+                entityRune.data[1] = 16;
                 break;
 
             case 300:
-                e.data[1] = 22;
+                entityRune.data[1] = 22;
                 break;
 
             case 400:
-                e.data[1] = 32;
+                entityRune.data[1] = 32;
                 break;
+		default:
+            entityRune.fizzle();
+            return;
         }
 //        List<Entity> ents = this.getEntities(e, 3D);
 //        boolean found = false;
@@ -116,39 +119,39 @@ public class RELiftTerrain extends RuneEvent
 //            e.fizzle();
 //            return;
 //        }
-        e.sacrificeWaiting = 600;
+        entityRune.sacrificeWaiting = 600;
         this.addSacrificeList(new Sacrifice(99));
 //        e.fade();
     }
 
-    public void onTick(EntityRune e)
+    public void onTick(EntityRune entityRune)
     {
 //        e.fade();
-        e.setColorStar(255, 255, 255);
+        entityRune.setColorStar(255, 255, 255);
 
-        if (e.ticksExisted < ticksperblock * 2)
+        if (entityRune.ticksExisted < ticksperblock * 2)
         {
             return;
         }
 
-        if (e.rutAreaPoints == null)
+        if (entityRune.rutAreaPoints == null)
         {
-            e.fade();
+            entityRune.fade();
         }
 
-        World world = e.worldObj;
-        int x = (int) e.getX();
-        int y = (int) e.getY();
-        int z = (int) e.getZ();
-        int height = e.data[1];
+        World world = entityRune.worldObj;
+        int x = (int) entityRune.getX();
+        int y = (int) entityRune.getY();
+        int z = (int) entityRune.getZ();
+        int height = entityRune.data[1];
 
-        if (e.ticksExisted % ticksperblock == 0 && e.data[2] <= height)
+        if (entityRune.ticksExisted % ticksperblock == 0 && entityRune.data[2] <= height)
         {
-        	if (e.rutAreaPoints == null) {
-        		loadArea(e);
+        	if (entityRune.rutAreaPoints == null) {
+        		loadArea(entityRune);
         	}
 //            int c = e.bb - e.gb;
-            for (Integer[] i : e.rutAreaPoints)
+            for (Integer[] i : entityRune.rutAreaPoints)
             {
                 x = i[0];
                 y = i[1];
@@ -166,29 +169,29 @@ public class RELiftTerrain extends RuneEvent
 
                 for (int t = -height; t <= height; t++)
                 {
-                    int c = -t + e.data[2] - 1;
+                    int c = -t + entityRune.data[2] - 1;
 
                     if (y + c <= 0)
                     {
-                        e.fade();
+                        entityRune.fade();
                         return;
                     }
 
                     if (t != height)
                     {
-                        Block B = world.getBlock(x, y + c, z);
+                        Block block = world.getBlock(x, y + c, z);
                         int m = world.getBlockMetadata(x, y + c, z);
                         Block nB = world.getBlock(x, y + c + 1, z);
 
 //                        System.out.println("fuck it all " + nb + " " + b + " " + world.getBlockId(x,y+c+2,z));;
-                        if (world.isAirBlock(x, y + c + 2, z) && B.getMaterial() != Material.air)
+                        if (world.isAirBlock(x, y + c + 2, z) && block.getMaterial() != Material.air)
                         {
 //                            System.out.println("GOOOOOOO");
-                            List<Entity> ents = getEntities(e.worldObj, (double) x + 0.5D, (double) y + (double)c + 1D, (double) z + 0.5D, 1D);
+                            List<Entity> ents = getEntities(entityRune.worldObj, (double) x + 0.5D, (double) y + (double)c + 1D, (double) z + 0.5D, 1D);
 
                             for (Entity ie : ents)
                             {
-                                if (ie == e)
+                                if (ie == entityRune)
                                 {
                                     continue;
                                 }
@@ -205,19 +208,19 @@ public class RELiftTerrain extends RuneEvent
                         }
 
                         boolean isContainer = false;
-                        TileEntity te = null;
+                        TileEntity tileEntity = null;
                         NBTTagCompound tag = null;
 
                         //                    System.out.println("lift " + b + " " + (B != null && B instanceof BlockContainer) + " " + (nB == null || !(nB instanceof BlockContainer)));
-                        if ((B != null && B instanceof BlockContainer)/* && (nB == null || !(nB instanceof BlockContainer))*/)
+                        if ((block != null && block instanceof BlockContainer)/* && (nB == null || !(nB instanceof BlockContainer))*/)
                         {
 //                            System.out.println("IS CONTAINER************************");
                             isContainer = true;
-                            te = world.getTileEntity(x, y + c, z);
+                            tileEntity = world.getTileEntity(x, y + c, z);
                             tag = new NBTTagCompound();
-                            te.writeToNBT(tag);
+                            tileEntity.writeToNBT(tag);
 //                            world.removeBlockTileEntity(x, y+c, z);
-                            te.invalidate();
+                            tileEntity.invalidate();
                             //                        world.setBlockTileEntity(x,y+c,z,null);
                             //                        Chunk chunk = e.worldObj.getChunkFromBlockCoords(e.getX(), e.getZ());
                             //                        chunk.setChunkBlockTileEntity(x & 0xf, y-t+e.gb, z & 0xf, null);
@@ -231,7 +234,7 @@ public class RELiftTerrain extends RuneEvent
                         //                        return;
                         //                    }
                         world.setBlockToAir(x, y + c + 1, z);
-                        world.setBlock(x, y + c + 1, z, B, m,3);
+                        world.setBlock(x, y + c + 1, z, block, m,3);
                         world.setBlock(x, y + c, z, Blocks.stone,0,3);
 
                         if (isContainer)
@@ -281,28 +284,28 @@ public class RELiftTerrain extends RuneEvent
                 }
             }
 
-            e.data[2]++;
+            entityRune.data[2]++;
 
 //            System.out.println("Data " + e.gb + " " + height);
-            if (e.data[2] >= height)
+            if (entityRune.data[2] >= height)
             {
-                e.fade();
+                entityRune.fade();
             }
         }
     }
 
-    private void loadArea(EntityRune e)
+    private void loadArea(EntityRune entityRune)
     {
-        this.findRutAreaFlat(e, Blocks.clay);
-        int min = e.getY();
+        this.findRutAreaFlat(entityRune, Blocks.clay);
+        int min = entityRune.getY();
         int max = 0;
 
-        for (int iter = 0; iter < e.rutAreaPoints.size(); iter++)
+        for (int iter = 0; iter < entityRune.rutAreaPoints.size(); iter++)
         {
-            Integer[] i = e.rutAreaPoints.get(iter);
+            Integer[] i = entityRune.rutAreaPoints.get(iter);
             int x = i[0];
             int z = i[1];
-            int h = e.worldObj.getHeightValue(x, z);
+            int h = entityRune.worldObj.getHeightValue(x, z);
 
             if (h < min)
             {
@@ -316,34 +319,34 @@ public class RELiftTerrain extends RuneEvent
 
 //            i[1] = e.worldObj.getHeightValue(i[0], i[2]);
 //            if(i[1] > max) max = i[1];
-            e.rutAreaPoints.set(iter, new Integer[] {x, h, z});
+            entityRune.rutAreaPoints.set(iter, new Integer[] {x, h, z});
 //            e.worldObj.setBlockWithNotify(i[0], e.worldObj.getHeightValue(i[0], i[1]), i[1], Block.glass.blockID);
         }
 
-        e.data[2] = max - min;
+        entityRune.data[2] = max - min;
 //        System.out.println("offset " + e.gb);
     }
 
     @Override
-    public void onUnload(EntityRune e)
+    public void onUnload(EntityRune entityRune)
     {
-        if (e.rutPoints == null)
+        if (entityRune.rutPoints == null)
         {
-            this.findRuts(e, Blocks.clay);
+            this.findRuts(entityRune, Blocks.clay);
         }
 
-        super.onUnload(e);
+        super.onUnload(entityRune);
 
-        if (e.rutPoints != null)
+        if (entityRune.rutPoints != null)
         {
-            for (Integer[] i : e.rutPoints)
+            for (Integer[] i : entityRune.rutPoints)
             {
-                int rand = e.worldObj.rand.nextInt(100);
+                int rand = entityRune.worldObj.rand.nextInt(100);
 
                 if (rand > 15)
                 {
 //                world.setBlockWithNotify(i[0], i[1], i[2], Block.melon.blockID);
-                    TileEntityRut ter = (TileEntityRut) e.worldObj.getTileEntity(i[0], i[1], i[2]);
+                    TileEntityRut ter = (TileEntityRut) entityRune.worldObj.getTileEntity(i[0], i[1], i[2]);
 
                     if (ter != null)
                     {
