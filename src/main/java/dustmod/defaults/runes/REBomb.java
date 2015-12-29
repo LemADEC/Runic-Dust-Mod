0714d86c7ce7f5ba4cffcbd9acd9b7b192f3ae2b
LemADEC <lemadec.fr@gmail.com>
2015-12-29 15:23:45 +0100

    Fixed rune of detonation
    Fixed dust variation to be more than gunpowder
    Fixed plant dust fuse triggering on itself
    Increased fuse duration span
    Added secret fun factor

--- src/main/java/dustmod/defaults/runes/REBomb.java
+++ src/main/java/dustmod/defaults/runes/REBomb.java
@@ -30,10 +30,10 @@ public class REBomb extends RuneEvent {
 	
 	@Override
 	public void onInit(EntityRune entityRune) {
-		ItemStack[] sac = new ItemStack[] { new ItemStack(Items.gunpowder, 2) };
-		sac = this.sacrifice(entityRune, sac);
+		ItemStack[] requiredSacrifice = new ItemStack[] { new ItemStack(Items.gunpowder, 2) };
+		requiredSacrifice = sacrifice(entityRune, requiredSacrifice);
 		
-		if (sac[0].stackSize > 0) {
+		if (requiredSacrifice[0].stackSize > 0) {
 			entityRune.fizzle();
 			return;
 		}
@@ -41,14 +41,14 @@ public class REBomb extends RuneEvent {
 		int[] center = new int[4];
 		int[] fuse = new int[4];
 		int[][] dusts = entityRune.dusts;
-		center[0] = dusts[3][1];
-		center[1] = dusts[4][1];
-		center[2] = dusts[3][2];
-		center[3] = dusts[4][2];
+		center[0] = dusts[2][1];
+		center[1] = dusts[3][1];
+		center[2] = dusts[2][2];
+		center[3] = dusts[3][2];
 		fuse[0] = dusts[0][4];
 		fuse[1] = dusts[1][4];
 		fuse[2] = dusts[1][3];
-		fuse[3] = dusts[2][3];
+		fuse[3] = dusts[0][5];
 		
 		for (int i = 0; i < 4; i++) {
 			if (center[0] != center[i]) {
@@ -62,47 +62,50 @@ public class REBomb extends RuneEvent {
 			}
 		}
 		
-		int c = center[0];
-		int f = fuse[0];
-		entityRune.data[0] = c;
-		entityRune.data[1] = f;
+		int fuseDuration = 0;
+		switch (fuse[0]) {
+		case 100:
+			fuseDuration = 1;
+			break;
+		case 200:
+			fuseDuration = 0 + entityRune.worldObj.rand.nextInt(5);
+			break;
+		case 300:
+			fuseDuration = 85 + entityRune.worldObj.rand.nextInt(10);
+			break;
+		case 400:
+			fuseDuration = 230 + entityRune.worldObj.rand.nextInt(20);
+			break;
+		default:
+			entityRune.fizzle();
+			return;
+		}
+		
+		entityRune.data[0] = center[0];
+		entityRune.data[1] = fuseDuration;
 		entityRune.setRenderStar(true);
 	}
 	
 	@Override
-	public void onTick(EntityRune e) {
-		int f = getTime(e.data[1]);
-		int c = e.data[0];
-		e.setRenderStar(true);
+	public void onTick(EntityRune entityRune) {
+		int center = entityRune.data[0];
+		int fuseDuration = entityRune.data[1];
 		
-		if (e.ticksExisted < f * 30) {
-			e.setColorStarInner(140, 140, 140);
-			e.setColorStarOuter(140, 140, 140);
+		entityRune.setRenderStar(true);
+		
+		if (entityRune.ticksExisted < fuseDuration) {
+			entityRune.setColorStarInner(140, 140, 140);
+			entityRune.setColorStarOuter(140, 140, 140);
 			return;
 		}
 		
-		e.setColorStarInner(0, 0, 255);
-		e.setColorStarOuter(0, 0, 255);
-		List<Entity> entities = getEntities(e);
+		entityRune.setColorStarInner(0, 0, 255);
+		entityRune.setColorStarOuter(0, 0, 255);
+		List<Entity> entities = getEntitiesExcluding(entityRune, 1.0D);
 		
-		if (entities.size() > 0 || f > 1) {
-			trigger(e, c);
-			e.fade();
-		}
-	}
-	
-	public int getTime(int f) {
-		switch (f) {
-		case 100:
-			return 1;
-		case 200:
-			return 2;
-		case 300:
-			return 3;
-		case 400:
-			return 4;
-		default:
-			return 1;
+		if (entities.size() > 0 || fuseDuration > 1) {
+			trigger(entityRune, center);
+			entityRune.fade();
 		}
 	}
 	
