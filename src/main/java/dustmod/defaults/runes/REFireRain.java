903414ad535fc72fa0202f55249320794388585b
LemADEC <lemadec.fr@gmail.com>
2015-12-29 17:19:42 +0100

    Code cleanup

--- src/main/java/dustmod/defaults/runes/REFireRain.java
+++ src/main/java/dustmod/defaults/runes/REFireRain.java
@@ -6,7 +6,6 @@ package dustmod.defaults.runes;
 
 import net.minecraft.entity.projectile.EntityArrow;
 import net.minecraft.init.Items;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import dustmod.runes.EntityRune;
 import dustmod.runes.PoweredEvent;
@@ -15,77 +14,69 @@ import dustmod.runes.PoweredEvent;
  *
  * @author billythegoat101
  */
-public class REFireRain extends PoweredEvent
-{
-    public REFireRain()
-    {
-        super();
-    }
+public class REFireRain extends PoweredEvent {
+	public REFireRain() {
+		super();
+	}
 	
 	@Override
-    public void initGraphics(EntityRune e){
-    	super.initGraphics(e);
-
-		e.setRenderBeam(true);
-        e.setColorStarOuter(255, 0, 0);
-        e.setColorBeam(255, 0, 0);
-    	
-    }
-
-    public void onInit(EntityRune e)
-    {
-        super.onInit(e);
-		e.setRenderBeam(true);
-        e.setColorBeam(255, 0, 0);
-        ItemStack[] req = new ItemStack[] {new ItemStack(Items.blaze_rod, 2)};
-        req = this.sacrifice(e, req);
-
-        if (!checkSacrifice(req))
-        {
-            e.fizzle();
-            return;
-        }
-    }
-
-    public void onTick(EntityRune e)
-    {
-        super.onTick(e);
-        int rad = 100;
-        int amt = 20;
-
-        for (int i = 0; i < amt && e.ticksExisted % 5 == 0; i++)
-        {
-            EntityArrow ea = new EntityArrow(e.worldObj, e.posX + Math.random() * rad * 2 - rad, 158, e.posZ + Math.random() * rad * 2 - rad);
-            ea.motionX = 0;
-            ea.motionY = -2D;
-            ea.motionZ = 0;
-            ea.setFire(100);
-            ea.canBePickedUp = 0;
-            e.worldObj.spawnEntityInWorld(ea);
-        }
-    }
-
-    @Override
-    public int getStartFuel()
-    {
-        return dayLength / 2;
-    }
-
-    @Override
-    public int getMaxFuel()
-    {
-        return dayLength * 3;
-    }
-
-    @Override
-    public int getStableFuelAmount(EntityRune e)
-    {
-        return dayLength / 2;
-    }
-
-    @Override
-    public boolean isPaused(EntityRune e)
-    {
-        return false;
-    }
+	public void initGraphics(EntityRune entityRune) {
+		super.initGraphics(entityRune);
+		
+		entityRune.setRenderBeam(true);
+		entityRune.setColorStarOuter(255, 0, 0);
+		entityRune.setColorBeam(255, 0, 0);
+	}
+	
+	@Override
+	public void onInit(EntityRune entityRune) {
+		super.onInit(entityRune);
+		entityRune.setRenderBeam(true);
+		entityRune.setColorBeam(255, 0, 0);
+		ItemStack[] req = new ItemStack[] { new ItemStack(Items.blaze_rod, 2) };
+		req = this.sacrifice(entityRune, req);
+		
+		if (!checkSacrifice(req)) {
+			entityRune.fizzle();
+			return;
+		}
+	}
+	
+	@Override
+	public void onTick(EntityRune entityRune) {
+		super.onTick(entityRune);
+		
+		int radius = 100;
+		int amount = 20;
+		
+		for (int i = 0; i < amount && entityRune.ticksExisted % 5 == 0; i++) {
+			EntityArrow entityArrow = new EntityArrow(entityRune.worldObj, entityRune.posX + (Math.random() * 2 - 1) * radius, 158, entityRune.posZ + (Math.random() * 2 - 1) * radius);
+			entityArrow.motionX = 0;
+			entityArrow.motionY = -2D;
+			entityArrow.motionZ = 0;
+			entityArrow.setFire(100);
+			entityArrow.canBePickedUp = 0;
+			entityRune.worldObj.spawnEntityInWorld(entityArrow);
+		}
+	}
+	
+	@Override
+	public int getStartFuel() {
+		return dayLength / 2;
+	}
+	
+	@Override
+	public int getMaxFuel() {
+		return dayLength * 3;
+	}
+	
+	@Override
+	public int getStableFuelAmount(EntityRune entityRune) {
+		return dayLength / 2;
+	}
+	
+	@Override
+	public boolean isPaused(EntityRune entityRune) {
+		return false;
+	}
 }
